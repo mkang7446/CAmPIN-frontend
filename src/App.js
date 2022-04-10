8965b93361251d5526c76698d01f9a5a691fa288
mkang7446 <mkang7446@gmail.com>
2022-04-10 12:43:49 -0700

    scaffold

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './screens/Home';
+import Mycampin from './screens/Mycampin';
+import Post from './screens/Post';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className='App'>
+    <Router>
+      <Routes>
+        <Route path='' element={<Home />} />
+        <Route path='/mycampin' element={<Mycampin />} />
+        <Route path='/post' element={<Post />} />
+      </Routes>
+    </Router>
+    // </div>
   );
 }
 
---
+++ src/screens/Mycampin.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Mycampin(props) {
+  return (
+    <div>
+      <h1>My CAmPIN</h1>
+    </div>
+  );
+}
+
+export default Mycampin;
